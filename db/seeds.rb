aa625f2a89b80221d4a2e16ff59a2f1b8765b558
dcouch440 <dcouch440@gmail.com>
2021-03-19 17:46:51 -0700

    remove notes

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,24 +3,6 @@ require 'faker'
 Product.destroy_all
 Review.destroy_all
 
-# Faker::Games::WorldOfWarcraft.quote
-# Faker::Number.within(range: 1..5)
-
-# Faker::Name.name
-
-# Faker::Device.build_number #=> "5"
-
-# Faker::Device.manufacturer #=> "Apple"
-
-# Faker::Device.model_name #=> "iPhone 4"
-
-# Faker::Commerce.price(range: 0..25.0)
-# Faker::Commerce.product_name
-
-# Faker::Lorem.sentence(word_count: 170, random_words_to_add:80)
-
-# Faker::Address.country
-
 50.times do
   @product = Product.create!(
     name: Faker::Commerce.product_name,
