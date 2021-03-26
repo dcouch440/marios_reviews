f4d936f45ac6d981ad878ad1d0fb77001c4523e8
dcouch440 <dcouch440@gmail.com>
2021-03-26 14:52:55 -0700

    set controller routes for user and admin roles

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,13 @@
 class ReviewsController < ApplicationController
 
+  before_action only: %i[ edit update destroy ] do
+    redirect_to products_review_path unless current_user&.admin
+  end
+
+  before_action only: %i[ new create ] do
+    redirect_to products_review_path unless current_user
+  end
+
   def new
     @product = Product.find(params[:product_id])
 
@@ -35,33 +43,23 @@ class ReviewsController < ApplicationController
 
   def update
     @review = Review.find(params[:id])
-
-    if @review.user.id !== user.id
-
-      if @review.update(review_params)
-        redirect_to product_path(@review.product)
-      else
-        @product = Product.find(params[:product_id])
-        render :edit
-      end
-
+    if @review.update(review_params)
+      redirect_to product_path(@review.product)
     else
-
-      redirect_to products_path
+      @product = Product.find(params[:product_id])
+      render :edit
     end
   end
 
   def destroy
     @review = Review.find(params[:id])
-    if @review.user_id == current_user&.id
-      @review.destroy
-    end
+    @review.destroy
     redirect_to product_path(@review.product)
   end
 
   private
-  def review_params
-    params.require(:review).permit(:author, :content_body, :rating, :user_id)
-  end
+    def review_params
+      params.require(:review).permit(:author, :content_body, :rating)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#index'
-  authenticate
+
   resources :products do
     resources :reviews
   end
+
+  
 end
